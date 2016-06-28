66bd806854f54a43d605fd2458d2735c82bbd745
mersocarlin <mersocarlin@gmail.com>
2016-06-28 21:41:08 +0200

    Add message presenter

---
+++ src/presenter/message.js
@@ -0,0 +1,25 @@
+export default function (data) {
+  if (Array.isArray(data)) {
+    return list(data);
+  }
+
+  return detail(data);
+}
+
+
+function list (messages) {
+  const result = messages.map(message => detail(message));
+
+  return result;
+}
+
+
+function detail (message) {
+  return {
+    _id: message._id,
+    name: message.name,
+    subject: message.subject,
+    content: message.content,
+    createdAt: message.createdAt,
+  };
+}
