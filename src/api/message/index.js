0fec3b29493f18f800cd7501788acf3fad7a6a0f
mersocarlin <mersocarlin@gmail.com>
2016-06-28 21:53:08 +0200

    Execute CRUD operations for each route type

--- src/api/message/index.js
+++ src/api/message/index.js
@@ -1,19 +1,71 @@
-export async function list (req, res) {
-  res.send(200, []);
-}
+import presenter from '../../presenter/message';
+import messageRepository from '../../repository/message';
+import * as message from '../../services/message';
 
-export async function detail (req, res) {
-  res.send(200, {});
+
+export async function list (req, res) {
+  res.send(
+    200,
+    presenter(
+      await message.list(
+        getRepositories()
+      )
+    )
+  );
 }
 
 export async function create (req, res) {
-  res.send(201, {});
+  res.send(
+    200,
+    presenter(
+      await message.create(
+        getRepositories(),
+        req.body,
+      )
+    )
+  );
+}
+
+export async function detail (req, res) {
+  res.send(
+    200,
+    presenter(
+      await message.detail(
+        getRepositories(),
+        req.params.id,
+      )
+    )
+  );
 }
 
 export async function update (req, res) {
-  res.send(200, {});
+  req.body.id = req.params.id; // eslint-disable-line no-param-reassign
+
+  res.send(
+    200,
+    presenter(
+      await message.update(
+        getRepositories(),
+        req.body,
+      )
+    )
+  );
 }
 
 export async function remove (req, res) {
-  res.send(200);
+  res.send(
+    200,
+    presenter(
+      await message.remove(
+        getRepositories(),
+        req.params.id,
+      )
+    )
+  );
+}
+
+function getRepositories () {
+  return {
+    message: messageRepository(),
+  };
 }
