7b83518309e824ecb5a5fd8f9c03dbbfef164da5
mersocarlin <mersocarlin@gmail.com>
2016-07-04 11:25:46 +0200

    Update build flow

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,12 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 
 export default (app) => {
-  app.get('/messages', wrap(message.list));
-  app.get('/messages/:id', wrap(message.detail));
-  app.post('/messages', wrap(message.create));
-  app.post('/messages/:id', wrap(message.update));
-  app.put('/messages/:id', wrap(message.update));
-  app.del('/messages/:id', wrap(message.remove));
+  app.get('api/messages', wrap(message.list));
+  app.get('api/messages/:id', wrap(message.detail));
+  app.post('api/messages', wrap(message.create));
+  app.post('api/messages/:id', wrap(message.update));
+  app.put('api/messages/:id', wrap(message.update));
+  app.del('api/messages/:id', wrap(message.remove));
 
 
   app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
