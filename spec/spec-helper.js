8d1c4a8f9ccd0c624aa6894f0ab7d20255ed0146
mersocarlin <mersocarlin@gmail.com>
2016-07-13 08:14:41 +0200

    Mock repository

---
+++ spec/spec-helper.js
@@ -0,0 +1,19 @@
+import messageRepository from './repository/message';
+
+
+export function createBigString () {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 10000; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+export function getRepositories () {
+  return {
+    message: messageRepository(),
+  };
+}
