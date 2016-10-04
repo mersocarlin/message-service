426a3a83c6a81dcd682376574c1cf41ab3270fd4
mersocarlin <mersocarlin@gmail.com>
2016-10-04 22:32:37 +0200

    Remove 'let' declaration from initial script

--- bin/service.js
+++ bin/service.js
@@ -3,7 +3,7 @@ require('babel-polyfill');
 
 require('../db/config').startDB(require('../config').config)
   .then(() => {
-    let app = require('../src/');
+    var app = require('../src/');
     app.start(require('../config').config)
     .catch(function (error) {
       console.error(error);
